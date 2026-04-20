4d299cac8ae1395ff6599bddb126f77f32eb4c02
Vishwanath-B-M <vishwanath99bm@gmail.com>
2026-04-20 16:53:40 +0530

    2nd commit

---
+++ src/pages.jsx/home.jsx
@@ -0,0 +1,42 @@
+import { useState,useEffect } from "react"
+import { Circles } from "react-loader-spinner";
+import ProductTile from './Users/vishwanath/Documents/shopping cart/shopping cart/src/component.jsx/products/index.jsx' 
+export default function Home(){
+     const [product,setproduct]=useState()
+     const [loading,setloading]=useState(false)
+
+
+  async function FetchProduct(){
+    setloading(true)
+    const res=await fetch('https://fakestoreapi.com/products')
+    const data= await res.json()
+    console.log(data)
+    if(data){
+        setloading(false)
+        setproduct(data)
+    }
+  }
+useEffect(()=>{
+    FetchProduct()
+},[])
+
+
+    return(<>
+    <div>{
+        loading ? <div className="min-h-screen w-full flex justify-center items-center">
+            <Circles
+            height={'120'}
+            width={'120'}
+            color="rgb(127,29,29)"
+            visible={true}
+            />
+        </div> : <div>
+            {
+                product&&product.product? 
+                product.map((productitem)=> <ProductTile product={productitem}/>):null
+            }
+
+            </div>}
+    </div>
+    </>)
+}
