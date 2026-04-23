bab6ffd09ae37e494581b84a5a8c35b82db1d1e9
Vishwanath-B-M <vishwanath99bm@gmail.com>
2026-04-23 16:58:13 +0530

    commited

--- src/component/products/index.jsx
+++ src/component/products/index.jsx
@@ -23,10 +23,11 @@ const cart=useSelector(state=>state.cart)
 
 
     return(<>
-    <div>
-        <img src={product.image} alt={product.title}/>
-        <div>
-          <button  onClick={() => isitem ? RemoveHandle() : HandleAddtoCart()} className="w-20 h-20 bg-green-900 rounded">
+    <div className='flex flex-col items-center border-2 border-red-900 mt-4 gap-4 rounded-xl'>
+        <img src={product.image} alt={product.title} className='object-cover h-full w-full'/>
+        <h1 className='w-40 truncate text-grey-700 font-bold'> {product.title}</h1>
+        <div className='flex item-center justify-center w-full mb-3'>
+          <button  onClick={() => isitem ? RemoveHandle() : HandleAddtoCart()} className="bg-green-700 rounded p-6 font-bold border-2 text-2xl">
              { isitem ? "remove from the cart":"add to cart" }
           </button>
         </div>
