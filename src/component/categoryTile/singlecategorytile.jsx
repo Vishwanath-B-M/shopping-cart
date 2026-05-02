d21d58fbb44cf951fbc2d5992bd6e83b3b54d850
Vishwanath-B-M <vishwanath99bm@gmail.com>
2026-05-02 16:42:13 +0530

    added

--- src/component/categoryTile/singlecategorytile.jsx
+++ src/component/categoryTile/singlecategorytile.jsx
@@ -12,13 +12,23 @@ export default function SingleCate({product}){
   }
   const isitem=cart.some((item)=>item.id===product.id)
     return(<>
-    <div className="lg:max-h-[600px] overflow-hidden flex flex-col items-center border-2 border-red-900 mt-8 gap-4 rounded-2xl border-2 border-black shadow-3xl hover:scale-105 transition-transform duration-500 
+    <div className="lg:max-h-[800px] overflow-hidden flex flex-col items-center border-2 border-red-900 mt-2 gap-2 rounded-2xl border-2 border-black shadow-3xl hover:scale-105 transition-transform duration-500 
     sm:max-h-100 flex flex-col items-center border-1 border-red-900 mt-2 gap-2 rounded border-2 border-white shadow-3xl hover:scale-105 transition-transform duration-500 ">
-      <img src={product.thumbnail} alt={product.title} className="object-cover h-1/2 w-full text-2xl"/>
-      <h3 className="w-40 truncate text-grey-700 font-bold p-1 text-3xl">{product.title}</h3>
-      <p className="text-grey-700 font-bold p-1 text-3xl ">{product.availabilityStatus}</p>
-      <p className="text-grey-700 font-bold p-1 text-3xl ">${product.price}</p>
-      <button onClick={()=>isitem?RemoveFromCart():HandleAddtoCart()} className="lg:w-72 h-25 gap-1 text-black text-4xl bg-white/40 mb-2 border-4 border-white/40 rounded-3xl shadow-lg  hover:backdrop-blur-2xl hover:text-5xl object-cover">{isitem?"remove from cart":"add to cart"}</button>
+      <img src={product.thumbnail} alt={product.title} 
+      className="object-cover h-[500px] w-full text-2xl"/>
+      <h3 className="w-40 truncate text-grey-700 font-bold pb-5 text-3xl">
+        {product.title}
+        </h3>
+      <p className="text-grey-700 font-bold  text-3xl ">
+        {product.availabilityStatus}
+        </p>
+      <p className="text-grey-700 font-bold text-3xl ">
+        ${product.price}
+        </p>
+      <button onClick={()=>isitem?RemoveFromCart():HandleAddtoCart()} 
+      className="lg:w-72 h-25 gap-1 text-black text-4xl bg-white/40 mb-2 border-4 border-white/40 rounded-3xl shadow-lg  hover:backdrop-blur-2xl hover:text-5xl object-cover">
+        {isitem?"remove from cart":"add to cart"}
+        </button>
     </div>
     
     
--- src/component/products/index.jsx
+++ src/component/products/index.jsx
@@ -22,24 +22,24 @@ const cart =useSelector(state=>state.cart.cart)
 
 
     return(<div>
-      <div className='flex flex-col h-[400px] items-center border-2 border-white mt-4 gap-2 rounded-xl shadow-xl hover:scale-105 transition-transform duration-500 overflow-hidden'>
+      <div className='flex flex-col h-[700px] items-center justify-center border-2 border-white mt-2 gap-1 rounded-xl shadow-xl hover:scale-105 transition-transform duration-500 overflow-hidden'>
 
 <img 
   src={product.images[0]} 
   alt={product.title} 
-  className='object-cover h-3/4 w-full'
+  className='object-cover h-[400px] w-full'
 />
 
-<h1 className='w-full truncate text-gray-700 font-bold text-lg text-center'>{product.title}</h1>
+<h1 className='w-40 truncate text-gray-700 font-bold p-4 text-2xl text-center'>{product.title}</h1>
 
-<h4 className='text-gray-700 text-sm'>{product.availabilityStatus}</h4>
+<h4 className='text-gray-700 text-2xl'>{product.availabilityStatus}</h4>
 
-<h4 className='text-gray-700 text-sm'>${product.price}</h4>
+<h4 className='text-gray-700 text-2xl'>${product.price}</h4>
 
 <div className=' w-full flex justify-center'>
   <button 
     onClick={() => isitem ? RemoveHandle() : HandleAddtoCart()}
-    className="w-72 h-25 text-4xl bg-white/40 border-white rounded-3xl shadow hover:scale-105 transition mb-2"
+    className="w-72 h-25 text-4xl bg-white/40 border-white rounded-3xl shadow hover:scale-105 transition mb-1 p-4"
   >
     {isitem ? "Remove" : "Add to cart"}
   </button>
