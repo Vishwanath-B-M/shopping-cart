74c5303c450ac667391b843ed51a2e081b76f78b
Vishwanath-B-M <vishwanath99bm@gmail.com>
2026-05-02 17:52:16 +0530

    added

--- src/component/products/index.jsx
+++ src/component/products/index.jsx
@@ -22,31 +22,41 @@ const cart =useSelector(state=>state.cart.cart)
 
 
     return(<div>
-      <div className='flex flex-col max-h-[400px] items-center justify-center border-2 border-white mt-2 gap-1 rounded-xl shadow-xl hover:scale-105 transition-transform duration-500 overflow-hidden
-     lg:max-h-[400px] overflow-hidden flex flex-col items-center justify-center border-2 border-red-900 mt-2 gap-2 rounded-2xl border-2 border-black shadow-3xl hover:scale-105 transition-transform duration-500 
-    '>
+      <div className="flex flex-col max-h-[400px] items-center justify-between 
+border-2 border-black mt-2 gap-2 rounded-2xl shadow-xl 
+hover:scale-105 transition-transform duration-500 overflow-hidden">
 
-<img 
-  src={product.images[0]} 
-  alt={product.title} 
-  className='object-cover h-1/2 w-full'
-/>
+  <img 
+    src={product.images[0]} 
+    alt={product.title} 
+    className="object-cover h-48 w-full"
+  />
 
-<h1 className='w-40 truncate text-gray-700 font-bold  text-2xl text-center'>{product.title}</h1>
+  <h1 className="w-40 truncate text-gray-700 font-bold text-lg text-center">
+    {product.title}
+  </h1>
 
-<h4 className='text-gray-700 text-lg'>{product.availabilityStatus}</h4>
+  <h4 className="text-gray-600 text-sm">
+    {product.availabilityStatus}
+  </h4>
 
-<h4 className='text-gray-700 text-lg'>${product.price}</h4>
+  <h4 className="text-gray-800 text-lg font-semibold">
+    ${product.price}
+  </h4>
 
-<div className=' w-full flex justify-center'>
-  <button 
-    onClick={() => isitem ? RemoveHandle() : HandleAddtoCart()}
-    className="w-72 h-25 text-4xl bg-white/40 border-white rounded-3xl shadow hover:scale-105 transition mb-2 p-1 lg:w-1/2 h-10 gap-1 text-black text-4xl bg-white/40 mb-2 border-4 border-white/40 rounded-3xl shadow-lg  hover:backdrop-blur-2xl hover:text-5xl object-cover">
-        {isitem?"remove from cart":"add to cart"}
-   </button>
+  <div className="w-full flex justify-center px-2 pb-2">
+    <button 
+      onClick={() => isitem ? RemoveHandle() : HandleAddtoCart()}
+      className="w-full lg:w-1/2 h-10 text-sm bg-white border rounded-xl shadow 
+      hover:scale-105 transition">
+      
+      {isitem ? "Remove from cart" : "Add to cart"}
+    </button>
+  </div>
+
 </div>
+      
 
-</div>
-   
+
       </div>)
  }
