6b00948625998118ed4f3acc66193f6a06b661a1
Vishwanath-B-M <vishwanath99bm@gmail.com>
2026-05-07 14:57:54 +0530

    added

--- src/component/categoryTile/singlecategorytile.jsx
+++ src/component/categoryTile/singlecategorytile.jsx
@@ -21,7 +21,7 @@ hover:scale-105 transition-transform duration-500">
     className="object-cover lg:h-48 w-full"
   />
 
-  <h3 className="w-16 truncate text-gray-700 font-bold text-sm lg:text-lg text-center">
+  <h3 className="w-16 truncate text-gray-700 font-bold text-sm lg:text-lg text-center p-2">
     {product.title}
   </h3>
 
@@ -35,7 +35,7 @@ hover:scale-105 transition-transform duration-500">
 
   <button 
     onClick={() => isitem ? RemoveFromCart() : HandleAddtoCart()} 
-    className="w-2/4 h-8 text-sm bg-white border rounded-xl shadow 
+    className="w-2/4 h-10 text-sm bg-white border rounded-xl shadow 
     hover:scale-105 transition mb-2"
   >
     {isitem ? "Remove from cart" : "Add to cart"}
